e9b8614786d19d898e653171ec12dae31d806bed
JosueRodriguezr <peroreye@espol.edu.ec>
2023-08-20 19:47:51 -0500

    Direccion - coordenadas

--- app.js
+++ app.js
@@ -64,4 +64,25 @@ app.get('/coordenadas', async(req, res) => {
     res.status(500).json({ error: 'An error occurred' });
   }
 });
+// ontener coordenadas desde direccion
+app.get('/direccion', async (req, res) => {
+  const direccion = req.query.direccion; 
+  const apiUrl = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(direccion)}`;
+  try {
+    const response = await axios.get(apiUrl);
+    if (response.data && response.data.length > 0) {
+      const coordenadas = {
+        lat: response.data[0].lat,
+        lng: response.data[0].lon
+      };
+      res.json({ coordenadas });
+    } else {
+      res.status(404).json({ error: 'Coordenadas no encontradas para la dirección proporcionada' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Ocurrió un error' });
+  }
+});
+
 
